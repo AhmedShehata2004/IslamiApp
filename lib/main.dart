a43421c53fb05c8efff7b7a112431bd5531630f2
Mahm0udalaa <Mahmoud2003as4@gmail.com>
2025-09-15 09:03:24 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home/home_screen.dart';
+
+void main() {
+	runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+	const MyApp({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'Islami App',
+      debugShowCheckedModeBanner: false,
+			theme: ThemeData(
+				primarySwatch: Colors.blue,
+			),
+      initialRoute: "/",
+      routes: {
+        "/": (context)=> HomeScreen(),
+
+
+      },
+			home: const HomeScreen()
+		);
+	}
+}
